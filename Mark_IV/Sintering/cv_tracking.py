ac4caf10286d58ecc4cd55684155f17c92666061
JuddB <jbrockedgar@gmail.com>
2024-01-18 13:01:21 -0500

    Add raspberry pi camera capabilities

--- Mark_IV/Sintering/cv_tracking.py
+++ Mark_IV/Sintering/cv_tracking.py
@@ -1,10 +1,20 @@
 import cv2
+import os
+from time import sleep
+from picamera import PiCamera
 
 def find_correction():
     # Read image.
-    thresh_pixel = 10
-    img = cv2.imread('./imgs/sun_filter3.jpg', cv2.IMREAD_COLOR)
-    
+    thresh_pixel = 9
+
+    camera = PiCamera()
+    camera.start_preview()
+    sleep(2.0)
+    camera.capture('./imgs/sun.jpg')
+    camera.stop_preview()
+    img = cv2.imread('./imgs/sun.jpg', cv2.IMREAD_COLOR)
+    os.remove("./imgs/sun.jpg")
+
     # Convert to grayscale.
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     gray = cv2.resize(gray, (540,700), interpolation=cv2.INTER_LINEAR)
@@ -17,14 +27,14 @@ def find_correction():
     mid_width = int(width / 2)
     mid_height = int(height / 2) 
 
-    img = cv2.resize(img, (540,700), interpolation=cv2.INTER_LINEAR)
-    cv2.circle(img, maxLoc, 1, 0, 2)
-    cv2.circle(img, (mid_width - thresh_pixel, 350 - thresh_pixel), 1, 0, 2)
-    cv2.circle(img, (mid_width + thresh_pixel, 350 + thresh_pixel), 1, 0, 2)
-    cv2.circle(img, (270 + thresh_pixel, mid_height - thresh_pixel), 1, 0, 2)
-    cv2.circle(img, (270 - thresh_pixel, mid_height + thresh_pixel), 1, 0, 2)
-    cv2.imshow("Image", img)
-    cv2.waitKey(0)
+    # img = cv2.resize(img, (540,700), interpolation=cv2.INTER_LINEAR)
+    # cv2.circle(img, maxLoc, 1, 0, 2)
+    # cv2.circle(img, (mid_width - thresh_pixel, 350 - thresh_pixel), 1, 0, 2)
+    # cv2.circle(img, (mid_width + thresh_pixel, 350 + thresh_pixel), 1, 0, 2)
+    # cv2.circle(img, (270 + thresh_pixel, mid_height - thresh_pixel), 1, 0, 2)
+    # cv2.circle(img, (270 - thresh_pixel, mid_height + thresh_pixel), 1, 0, 2)
+    # cv2.imshow("Image", img)
+    # cv2.waitKey(0)
     
     pic_info = ["stay", "stay", maxVal, abs(mid_width - maxLoc[0]), abs(mid_height - maxLoc[1])]
     if mid_width + thresh_pixel < maxLoc[0]:
@@ -35,6 +45,7 @@ def find_correction():
         pic_info[1] = "down"
     elif mid_height - thresh_pixel > maxLoc[1]:
         pic_info[1] = "up"
+    print(pic_info)
     return pic_info
 
 def main():
--- Mark_IV/Sintering/trackingNew.py
+++ Mark_IV/Sintering/trackingNew.py
@@ -13,7 +13,7 @@ azVal = None
 class azimuth_tracker:
     def __init__(self):
         self.logger = logger()
-        self.maxVal = 254
+        self.maxVal = 20
  
     def stepMovement(self, direction, steps):
         GPIO.setwarnings(False)
@@ -96,14 +96,16 @@ class azimuth_tracker:
                 self.logger.logInfo("Azimuth Adjustment...")
                 
                 pic_info = find_correction()
+                az_dir = pic_info[0]
+                elev_dir = pic_info[1]
                 bright_val = pic_info[2]
                 
                 # Set the initial azimuth and elevation movement step numbers.
                 # When the sun is not in frame, move for these number of steps.
-                min_az_steps = 30
+                min_az_steps = 40
                 min_elev_steps = 4
-                max_az_steps = 300
-                max_elev_steps = 18
+                max_az_steps = 1200
+                max_elev_steps = 16
                 az_steps = max_az_steps
                 elev_steps = max_elev_steps
 
@@ -112,41 +114,40 @@ class azimuth_tracker:
 
                     # Set number of azimuth and elevations steps to take.
                     # Take more movement steps the farther from the middle of the pic the sun is located.
-                    az_steps = round(pic_info[3], 0)
-                    elev_steps = round(pic_info[4] / 20, 0)
+                    az_steps = int(round(pic_info[3] * 6, 0))
+                    elev_steps = int(round(pic_info[4] / 20, 0))
                     if az_steps < min_az_steps: az_steps = min_az_steps
                     elif az_steps > max_az_steps: az_steps = max_az_steps
                     if elev_steps < min_elev_steps: elev_steps = min_elev_steps
                     elif elev_steps > max_elev_steps: elev_steps = max_elev_steps
 
                     # Detemine direction of azimuth and elevation motors.
-                    az_dir = pic_info[0]
-                    elev_dir = pic_info[1]
                     run = True
                     # Set the azimuth direction.
                     if az_dir == "right":
-                        GPIO.output(DIR_1, 0)
-                    elif az_dir == "left":
                         GPIO.output(DIR_1, 1)
+                    elif az_dir == "left":
+                        GPIO.output(DIR_1, 0)
                     else:
                         run = False
 
                     # Get elevation direction and move.
                     if elev_dir == "up":
                         run = True
-                        GPIO.output(DIR, 1)
+                        GPIO.output(DIR, 0)
                     elif elev_dir == "down":
                         run = True
-                        GPIO.output(DIR, 0)
+                        GPIO.output(DIR, 1)
                         
                     # If neither azimuth or elevation needed to move, exit since it found the sun.
                     if not run:
                         return
                     
                     if elev_dir != "stay":
+                        print(elev_steps)
                         for x in range(elev_steps):
                             GPIO.output(STEP, GPIO.HIGH)
-                            sleep(0.05)  # Dictates how fast stepper motor will run
+                            sleep(0.15)  # Dictates how fast stepper motor will run
                             GPIO.output(STEP, GPIO.LOW)
                 
                 # Move azimuth as long as the sun is not in the middle of the image.
@@ -155,9 +156,10 @@ class azimuth_tracker:
                         GPIO.output(STEP_1, GPIO.HIGH)
                         # .5 == super slow
                         # .00005 == breaking
-                        sleep(0.001)
+                        sleep(0.0011)
                         GPIO.output(STEP_1, GPIO.LOW)
-                        sleep(0.001)
+                        sleep(0.0011)
+                # sleep(1.0)
 
         # Once finished clean everything up
         except Exception as e:
@@ -291,7 +293,8 @@ class azimuth_tracker:
 
 def main():
     at = azimuth_tracker()
-    at.stepMovement(1, 100)
+    # at.stepMovement(1, 100)
+    at.tracking()
 
 if __name__ == "__main__":
     main()
